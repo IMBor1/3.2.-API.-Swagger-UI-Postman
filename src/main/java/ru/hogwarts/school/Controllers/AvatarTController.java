57072083e7f106a6855c9dd3cf10bb55c6a26733
IMBor1 <ilacerber1@gmail.com>
2024-04-23 22:52:15 +0300

    4.1. готово

--- src/main/java/ru/hogwarts/school/Controllers/AvatarTController.java
+++ src/main/java/ru/hogwarts/school/Controllers/AvatarTController.java
@@ -1,5 +1,27 @@
 package ru.hogwarts.school.Controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.Model.Avatar;
+import ru.hogwarts.school.Service.AvatarService;
+
+import java.util.List;
+
+@RestController
 public class AvatarTController {
+    AvatarService avatarService;
+
+    public AvatarTController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @GetMapping("avatar/all")
+    ResponseEntity<List<Avatar>> getAvatars(@RequestParam("page") Integer pageNumber,
+                                            @RequestParam("size") Integer pageSize) {
+        List<Avatar> avatars = avatarService.getAllAvatars(pageNumber, pageSize);
+        return ResponseEntity.ofNullable(avatars);
+    }
 
 }
