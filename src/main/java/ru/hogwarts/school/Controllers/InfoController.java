17396676722400faaecec97580decc597a3be9a4
IMBor1 <ilacerber1@gmail.com>
2024-05-07 23:09:58 +0300

    4.5 готово.

--- src/main/java/ru/hogwarts/school/Controllers/InfoController.java
+++ src/main/java/ru/hogwarts/school/Controllers/InfoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hogwarts.school.Service.InfoService;
 
+import java.util.List;
+
 @RestController
 public class InfoController {
     @Autowired
@@ -17,7 +19,7 @@ public class InfoController {
     }
 
     @GetMapping("/sum")
-    public long sumModdif() {
+    public List<Long> sumModdif() {
         return infoService.sumModdif();
     }
 }
--- src/main/java/ru/hogwarts/school/Service/InfoService.java
+++ src/main/java/ru/hogwarts/school/Service/InfoService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @Service
@@ -16,24 +18,31 @@ public class InfoService {
         return port;
     }
 
-    public long sumModdif() {
+    public List<Long> sumModdif() {
         Logger logger = LoggerFactory.getLogger(InfoService.class);
-        long startTime = System.currentTimeMillis();
-
-        int sum = Stream.iterate(1, a -> a + 1)
-
-                .limit(1_000_000)
-                .reduce(0, (a, b) -> a + b);
-        long time = System.currentTimeMillis() - startTime;
-        logger.info(time + "");
         long startTime1 = System.currentTimeMillis();
-
-        int sum1 = Stream.iterate(1, a -> a + 1)
-                .parallel()
-                .limit(1_000_000)
+        int sum = Stream.iterate(1, a -> a + 1)
+                .limit(1_000_0000)
                 .reduce(0, (a, b) -> a + b);
         long time1 = System.currentTimeMillis() - startTime1;
         logger.info(time1 + "");
-        return time;
+        long startTime2 = System.currentTimeMillis();
+// 9 из 10 раз этот метод самый быстрый.
+        int sum2 = IntStream.iterate(1, a -> a + 1)
+                .parallel()
+                .limit(1_000_0000)
+                .reduce(0, Integer::sum);
+        long time2 = System.currentTimeMillis() - startTime2;
+        logger.info(time2 + "");
+        long startTime3 = System.currentTimeMillis();
+
+        int sum3 = IntStream.iterate(1, a -> a + 1)
+                .parallel()
+                .limit(1_000_0000)
+                .sum();
+
+        long time3 = System.currentTimeMillis() - startTime3;
+        logger.info(time3 + "");
+        return List.of(time1, time2, time3);
     }
 }
