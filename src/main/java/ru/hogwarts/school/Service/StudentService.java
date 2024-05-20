f340b5ede70074c52e221e74c0c8ad31d36ec6f0
IMBor1 <ilacerber1@gmail.com>
2024-05-20 22:11:32 +0300

    добавил коменты

--- src/main/java/ru/hogwarts/school/Service/StudentService.java
+++ src/main/java/ru/hogwarts/school/Service/StudentService.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @Service
 public class StudentService {
     Logger logger = LoggerFactory.getLogger(StudentService.class);
+    //инжектим репозиторий
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
