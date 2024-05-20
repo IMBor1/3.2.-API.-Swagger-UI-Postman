bd62853addd124769d31f7f49fc6fefeca3bd9df
IMBor1 <ilacerber1@gmail.com>
2024-05-20 22:17:24 +0300

    коменты в студента

--- src/main/java/ru/hogwarts/school/Service/StudentService.java
+++ src/main/java/ru/hogwarts/school/Service/StudentService.java
@@ -21,27 +21,31 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
-
+    //созздал студента
     public Student createStudent(Student student) {
         logger.info("Was invoked method for create student");
         return studentRepository.save(student);
     }
 
+    //найти студента
     public Student findStudent(Long id) {
         logger.info("Was invoked method for find student");
         return studentRepository.findById(id).get();
     }
 
+    //изменить студента
     public Student editStudent(Student student) {
         logger.info("Was invoked method for edit student");
         return studentRepository.save(student);
     }
 
+    //удалить студента
     public void removeStudent(Long id) {
         logger.info("Was invoked method for delete student");
         studentRepository.deleteById(id);
     }
 
+    //список по возрасту
     public List<Student> listAge(Integer age) {
         logger.info("Was invoked method for get list by age student");
         return studentRepository.findAll().stream()
@@ -50,31 +54,37 @@ public class StudentService {
 
     }
 
+    //найти по возрасту в пределах
     public List<Student> findByAgeBetween(int minAge, int maxAge) {
         logger.info("Was invoked method for get list by ageBetween student");
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
+    //список всех студентов
     public List<Student> allStudents() {
         logger.info("Was invoked method for get list student");
         return studentRepository.findAll();
     }
 
+    //количество всех студентов
     public Integer getAllStudents() {
         logger.info("Was invoked method for get number of students");
         return studentRepository.getAllStudents();
     }
 
+    //среднее число студентов
     public Integer getAvgStudents() {
         logger.info("Was invoked method for get average age from student");
         return studentRepository.getAvgStudents();
     }
 
+    //последние 5 студентов
     public List<Student> getLastStudents() {
         logger.info("Was invoked method for get list last 5 students");
         return studentRepository.getLastStudents();
     }
 
+    //список имен начинающихся на "А"
     public ResponseEntity<List<String>> namesStartA() {
         List<String> students = studentRepository.findAll().stream()
                 .filter(s -> s.getName().toUpperCase().startsWith("A"))
@@ -85,13 +95,14 @@ public class StudentService {
         return ResponseEntity.ok(students);
     }
 
+    //среднее число через findAll
     public ResponseEntity<Double> getAverageAge() {
         double studentsAvAge = studentRepository.findAll().stream()
                 .mapToDouble(Student::getAge).average().orElseThrow();
-        //.collect(Collectors.toList());
         return ResponseEntity.ok(studentsAvAge);
     }
 
+    //все имена через разпараллеливание
     public List<String> allParallelNames() {
         List<String> names = studentRepository.findAll().stream()
                 .parallel()
@@ -108,6 +119,7 @@ public class StudentService {
         return names;
     }
 
+    //синхронизация
     public List<String> syncNames() {
         List<String> syncStudents = studentRepository.findAll().stream()
                 .parallel()
